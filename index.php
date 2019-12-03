8afb2f95b0f6053206bea9a6d41d1ebb6937032f
Adriano Souza <adrianosouza@aix.com.br>
2019-12-03 09:51:50 -0300

    incluindo 2 variaveis para com escala e sem escala.

--- index.php
+++ index.php
@@ -5,4 +5,7 @@ Melhor preço com escalas R$ 994 (1)
 
 1 - Incluindo todas as taxas.";
 
-var_dump(preg_match_all('((com escalas)+ (R\$) ([0-9]{1})?(\.)?[0-9]{3})', $text));
+$comEscala = preg_match_all('((com escalas)+ (R\$) ([0-9]{1})?(\.)?[0-9]{3})', $text);
+$semEscala = preg_match_all('((sem escalas)+ (R\$) ([0-9]{1})?(\.)?[0-9]{3})', $text);
+
+var_dump($comEscala, $semEscala);
